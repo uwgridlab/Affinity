bce9889c7e6a7087283e6b3ce9a79ab32046c88f
James Wu <jiwu@cs.washington.edu>
2016-05-31 15:18:37 -0700

    eliminate plotfreqs button and calls

--- makecontrols.js
+++ makecontrols.js
@@ -15,25 +15,10 @@ var slide = d3.select("#thresh");
 // define slider value
 var slideVal = parseFloat(slide.node().value)
 
-// define button to click to plot
-var buttonFreq = d3.select("#plotFreqs");
-
-// function to decide type
-var decideType = function() {
-    if (typeNum == "Real"){
-        numsRange = math.range(0,1);
-    } else if (typeNum == "Imag") {
-        numsRange =  math.range(1,2);
-
-    } else if (typeNum == "AbsVal") {
-        numsRange = math.range(0, 2)
-    }
-
-
-};
+// // define button to click to plot
+// var buttonFreq = d3.select("#plotFreqs");
 
 // update function for slider
-
 var updateSlide = function(thresh) {
 
     // adjust the text on the range slider
@@ -47,10 +32,8 @@ var updateSlide = function(thresh) {
 
 };
 
-
 // overall update function
 var update = function() {
-    decideType();
     freqRange = math.range(f1,f2);
     locsRange = math.range(0,numLocs);
     indexR =  math.index(freqRange,locsRange,locsRange,math.range(0,1));
@@ -60,11 +43,13 @@ var update = function() {
 
     if (typeNum == "AbsVal")
         subsetMatrix = math.sqrt(math.add(math.square(matrixR),math.square(matrixI)));
-    else if (typeNum = "Angle")
+    else if (typeNum = "Angle"){
         subsetMatrix = math.add(math.atan2(matrixI, matrixR), 2*math.pi);
+        
+    }
+
 
     matrixMean = math.squeeze(math.mean(subsetMatrix,0));
-
     matrixMeanArray = matrixMean.valueOf();
 
     if(showSelf == "NOshowSelf"){
@@ -72,18 +57,13 @@ var update = function() {
             matrixMeanArray[i][i] = 0;
         }
     }
-
     renderChord(regions_global, matrixMeanArray);
-
-
-
 };
 
 // read in data, using initial guys
 d3.json("dataFULL.txt", function(data) {
 
     //parse the JSON with the math.js reviver
-
     a = JSON.parse(data, math.json.reviver);
 
     // use math.js to make a matrix
@@ -94,15 +74,6 @@ d3.json("dataFULL.txt", function(data) {
 
     freqRange = math.range(f1,f2);
     locsRange = math.range(0,numLocs);
-
-    /*
-    decideType();
-
-    index = math.index(freqRange,locsRange,locsRange,numsRange);
-
-    subsetMatrix = matrixData.subset(index);
-    */
-
 
     // update button on click
     button
@@ -123,19 +94,12 @@ d3.json("dataFULL.txt", function(data) {
                 threshChords(+this.value);
             });
 
-    buttonFreq
-            .on("click",function(){
-                channel_1 = d3.select("#chan1").node().value;
-                channel_2 = d3.select("#chan2").node().value;
+    // buttonFreq
+    //         .on("click",function(){
+    //             channel_1 = d3.select("#chan1").node().value;
+    //             channel_2 = d3.select("#chan2").node().value;
 
-                updateFreqsPlot();
-            });
-
-
-
-    /*
-    update();
-    updateSlide(0);
-    */
+    //             updateFreqsPlot();
+    //         });
 
 });
