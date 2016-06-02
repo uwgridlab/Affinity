b51e1ba30f73262e163188355c9259d55d1ae263
David Caldwell <djcald@cs.washington.edu>
2016-06-01 19:03:42 -0700

    changes to bar chart on top of chord

--- bar.js
+++ bar.js
@@ -1,33 +1,56 @@
 // define bar graph
 
-// Define chord visualization
-var width = 960,
-    height = 500;
+// Define bar visualization
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear()
     .range([height, 0]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 var data = [1,2,3,4,5];
 
-var svgbar = d3.select("body").select("div.leftchart")
-    .append("svg")
-    .attr("width", widthbar)
-    .attr("height", heightbar)
-    .attr("id", "svgcirc")
+x.domain(data.map(function(d,i) { return i; }));
+y.domain([0, d3.max(data)]);
+
+var svgbar = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("id", "bar")
-    .attr("transform", "translate(" + widthbar / 2 + "," + heightbar / 2 + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svgbar.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svgbar.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+svgbar.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d,i) { return x(i); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d,i) { return y(i); })
+    .attr("height", function(d,i) { return height - y(i); });
+
 
 var plotBars = function(si,ti){
 
-    svgbar
-        .data(data)
-        .enter()
-        .append
 
 
 };
